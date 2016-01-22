f2889a6567691c43299767394c1f7172919242f8
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-01-22 18:19:36 +0100

    structure :

---
+++ src/main/java/com/gpjpe/domain/TweetsHTable.java
@@ -0,0 +1,12 @@
+package com.gpjpe.domain;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class TweetsHTable {
+
+    private final static Logger LOGGER = Logger.getLogger(TweetsHTable.class.getName());
+
+    private static final String TABLE_NAME = "Tweets";
+
+}
