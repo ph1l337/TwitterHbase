01ee9b059c68be864da145abf0508a3aa005184d
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-01-22 18:48:55 +0100

    schema file

---
+++ src/main/java/com/gpjpe/domain/Schema.java
@@ -0,0 +1,9 @@
+package com.gpjpe.domain;
+
+public class Schema {
+
+    public static final String TABLE_NAME = "Tweets";
+    public static final String CF_HT = "ht";
+    public static final String CF_META = "meta";
+    public static final String COLUMN_HT_LANG = "lang";
+}
