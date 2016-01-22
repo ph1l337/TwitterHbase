129fc82ba3fb8ca5ee261188a580ada956450dcf
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-01-22 19:27:40 +0100

    update pom

---
+++ src/main/java/com/gpjpe/App.java
@@ -0,0 +1,25 @@
+package com.gpjpe;
+
+import com.gpjpe.domain.TweetsHTable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+
+public class App {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(App.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        Configuration configuration = HBaseConfiguration.create();
+
+        TweetsHTable tweetsHTable = new TweetsHTable(configuration);
+
+        tweetsHTable.initializeSchema();
+
+    }
+}
