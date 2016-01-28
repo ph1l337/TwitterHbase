6af6c04a4f384895b8f621b03f2d8622f199aec6
Philipp <github@philippeisen.de>
2016-01-28 11:49:35 +0100

    fixed window etc.

--- src/main/java/com/gpjpe/helpers/Utils.java
+++ src/main/java/com/gpjpe/helpers/Utils.java
@@ -16,29 +16,42 @@ public class Utils {
 	}
 
 	public static byte[] generateKey(long timeStamp, String lang) {
-		byte[] key = new byte[10];
-		System.arraycopy(Bytes.toBytes(timeStamp), 0, key, 0, 8);
-		System.arraycopy(Bytes.toBytes(lang), 0, key, 8, 2);
+		byte[] key = new byte[13];
+
+		byte[] tsPart = Bytes.toBytes(timeStamp);
+		byte[] langPart = new byte[5];
+
+		//fill langPart with 0s
+		for (byte b : langPart){
+			langPart[b] = (byte) -255;
+		}
+		//fill up with chars
+		for(int i = 0; i < lang.toCharArray().length;i++){
+			langPart[i] = (byte) lang.toCharArray()[i];
+		}
+
+		System.arraycopy(tsPart, 0, key, 0, 8);
+		System.arraycopy(langPart, 0, key, 8, 5);
 		return key;
 	}
 
 	public static byte[] generateKey(long timeStamp, QueryKey queryKey) {
-		byte[] key = new byte[10];
+		byte[] key = new byte[13];
 		System.arraycopy(Bytes.toBytes(timeStamp), 0, key, 0, 8);
 		switch (queryKey) {
 		case start:
-			byte[] low = { (byte) -255, (byte) -255 };
-			System.arraycopy(low, 0, key, 8, 2);
+			byte[] low = { (byte) -255, (byte) -255,(byte) -255, (byte) -255,(byte) -255 };
+			System.arraycopy(low, 0, key, 8, 5);
 			break;
 		case end:
-			byte[] high = { (byte) 255, (byte) 255 };
-			System.arraycopy(high, 0, key, 8, 2);
+			byte[] high = { (byte) 255, (byte) 255,(byte) 255, (byte) 255,(byte) 255 };
+			System.arraycopy(high, 0, key, 8, 5);
 			break;
 		}
 		return key;
 	}
 
-	public static void writeToFile(String ouput, String outputFolder,
+	public static void writeToFile(String output, String outputFolder,
 			int queryId, String outPrefix) {
 		BufferedWriter writer = null;
 		try {
@@ -60,7 +73,7 @@ public class Utils {
 							outputFolder, outPrefix, queryId), true), Charset
 							.forName("UTF-8").newEncoder()));
 
-			writer.write(ouput);
+			writer.write(output);
 
 		} catch (IOException e) {
 			LOGGER.error(e.toString());
--- src/test/java/com/gpjpe/TestUtils.java
+++ src/test/java/com/gpjpe/TestUtils.java
@@ -2,20 +2,58 @@ package com.gpjpe;
 import com.gpjpe.helpers.Utils;
 import junit.framework.TestCase;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.file.tfile.ByteArray;
+
+import java.util.Arrays;
 
 
 public class TestUtils extends TestCase{
 
     public void testKeyGen(){
         long timestamp = 1453191726000L;
-        String lang = "en";
+        String lang = "mkd";
         byte[] tsPart = new byte[8];
-        byte[] langPart = new byte[2];
+        byte[] langPart = new byte[5];
+
+
+        System.out.println(Arrays.toString(Utils.generateKey(timestamp, lang)));
+
+
         System.arraycopy(Utils.generateKey(timestamp, lang),0, tsPart,0,8);
-        System.arraycopy(Utils.generateKey(timestamp, lang),8, langPart,0,2);
+        System.arraycopy(Utils.generateKey(timestamp, lang),8, langPart,0,5);
         System.out.println(Bytes.toLong(tsPart) + Bytes.toString(langPart));
-        assertEquals(Bytes.toLong(tsPart),timestamp);
-        assertEquals(Bytes.toString(langPart),lang);
+        assertEquals(Utils.generateKey(timestamp,lang).length,13);
+        //assertEquals(Bytes.toString(langPart),lang);
+
+    }
+    public void testKeyGenWithoutLangLow(){
+        long timestamp = 1453191726000L;
+        byte[] tsPart = new byte[8];
+        byte[] langPart = new byte[5];
+
+        System.out.println(Arrays.toString(Utils.generateKey(timestamp, Utils.QueryKey.start)));
+
+
+        System.arraycopy(Utils.generateKey(timestamp, Utils.QueryKey.start),0, tsPart,0,8);
+        System.arraycopy(Utils.generateKey(timestamp, Utils.QueryKey.start),8, langPart,0,5);
+        System.out.println(Bytes.toLong(tsPart) + Bytes.toString(langPart));
+        assertEquals(Utils.generateKey(timestamp,Utils.QueryKey.start).length,13);
+        //assertEquals(Bytes.toString(langPart),lang);
+
+    }
+    public void testKeyGenWithoutLangHigh(){
+        long timestamp = 1453191726000L;
+        byte[] tsPart = new byte[8];
+        byte[] langPart = new byte[5];
+
+        System.out.println(Arrays.toString(Utils.generateKey(timestamp, Utils.QueryKey.end)));
+
+        System.arraycopy(Utils.generateKey(timestamp, Utils.QueryKey.end),0, tsPart,0,8);
+        System.arraycopy(Utils.generateKey(timestamp, Utils.QueryKey.end),8, langPart,0,5);
+        System.out.println(Bytes.toLong(tsPart) + Bytes.toString(langPart));
+        assertEquals(Utils.generateKey(timestamp,Utils.QueryKey.end).length,13);
+        //assertEquals(Bytes.toString(langPart),lang);
 
     }
 }
+
